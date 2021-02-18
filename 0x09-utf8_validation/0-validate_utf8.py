9c5f981e08bc4af24609927a84529ac4e71acdc8
karim bahri <karim-bahri-18@outlook.fr>
2021-02-18 17:17:02 +0100

    utf8 validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@
 
 def validUTF8(data):
     """a method to check for utf8 validation"""
-    if type(data) != list:
+    if type(data) != list or not len(data):
         return False
     for element in data:
         if type(element) != int:
