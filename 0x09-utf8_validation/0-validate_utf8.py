db3ab8f18958ede5cf9eb3cd252d25ba2788f324
karim bahri <karim-bahri-18@outlook.fr>
2021-02-18 17:08:33 +0100

    utf8 validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""utf-8 validation"""
+
+
+def validUTF8(data):
+    """a method to check for utf8 validation"""
+    if type(data) != list:
+        return False
+    for element in data:
+        if type(element) != int:
+            return False
+        if element > 127:
+            return False
+    return True
