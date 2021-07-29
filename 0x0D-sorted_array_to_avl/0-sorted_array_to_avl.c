6287c798db04744f8dae63ff6dc14319c306e328
karim <karim-bahri-18@outlook.fr>
2021-07-29 12:23:59 +0100

    toAvl

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - convert sorted array to tree
+ * @array: array to convert
+ * @size: size of the array
+ * Return: AVL Tree
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+		if (!array)
+			return (NULL);
+		return (convertToAVL(array, NULL, 0, size - 1));
+}
+
+/**
+ * convertToAVL - recursive function to convert array to avl tree
+ * @array: array to convert
+ * @parent: root of the node
+ * @beg: start array
+ * @end: array breakpoint
+ * Return: AVL tree
+ */
+avl_t *convertToAVL(int *array, avl_t *parent, int beg, int end)
+{
+	avl_t *node = NULL;
+	int middle = (beg + end) / 2;
+
+
+	node = malloc(sizeof(avl_t));
+	if (!node)
+		return (NULL);
+
+	if (beg <= end)
+	{
+		node->n = array[middle];
+		node->parent = parent;
+		node->left = convertToAVL(array, node, beg, middle - 1);
+		node->right = convertToAVL(array, node, middle + 1, end);
+		return (node);
+	}
+	return (NULL);
+}
