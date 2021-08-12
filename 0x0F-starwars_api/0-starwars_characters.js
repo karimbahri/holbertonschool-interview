1c74535a50928ba9b08d440bd5bb1745155f4157
karim <karim-bahri-18@outlook.fr>
2021-08-12 15:38:30 +0100

    starwars with promises

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+const request = require('request');
+
+const getCharacterName = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else { resolve(JSON.parse(body).name); }
+    });
+  });
+};
+
+request('https://swapi-api.hbtn.io/api/films/', async (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const charactersAPI = JSON.parse(body).results[2].characters;
+  const characters = [];
+  for (const key in charactersAPI) {
+    characters.push(await getCharacterName(charactersAPI[key]));
+  }
+  for (const element of characters) {
+    console.log(element);
+  }
+});
