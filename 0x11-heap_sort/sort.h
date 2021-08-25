279361b463c5e614d5353cb1934d90bc89535d16
karim <karim-bahri-18@outlook.fr>
2021-08-25 15:42:04 +0100

    header betty

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -2,6 +2,6 @@
 #define __HEAP_SORT__
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void swap_int(int *_, int *__);
 void array_heapify(int *array, int index, int len, int size);
+void swap_int(int *_, int *__);
 #endif
