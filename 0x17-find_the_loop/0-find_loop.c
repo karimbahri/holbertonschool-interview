0b6e57a0c4b869a34485da4dc110fe2804ef92db
karim <karim-bahri-18@outlook.fr>
2021-10-06 15:00:59 +0100

    find loop

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - detect loop in linked list
+ * @head: list to check
+ * Return: reference of the first node if true otherwise NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	if (!slow)
+		return (NULL);
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (slow);
+	}
+
+	return (NULL);
+}
