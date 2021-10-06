618444bdb9d246b9cc60895b6b5e8984049a0532
karim <karim-bahri-18@outlook.fr>
2021-10-06 17:03:00 +0100

    find loop

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -11,7 +11,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head, *fast = head;
 
-	if (!slow || !slow->next || !slow->next->next)
+	if (!slow)
 		return (NULL);
 
 	while (fast && fast->next)
@@ -20,7 +20,15 @@ listint_t *find_listint_loop(listint_t *head)
 		fast = fast->next->next;
 
 		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
 			return (slow);
+		}
 	}
 
 	return (NULL);
