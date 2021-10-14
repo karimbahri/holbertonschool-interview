4eb92fdfaa2fb88abe7f518c08db210ff3ea04f9
karim <karim-bahri-18@outlook.fr>
2021-10-14 01:15:13 +0100

    merge

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void cpy(int *src, int *dest, int size)
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		dest[i] = src[i];
+	}
+}
+/**
+ * merge_sort - sorting array using merge_sort algorithm
+ * @array: array to sort
+ * @size: array's length
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *merged = NULL;
+
+	if (!array)
+		return;
+	if (size < 2)
+		return;
+	
+	merged = malloc(size * sizeof(int));
+	if (!merged)
+		return;
+
+	shift(array, merged, size);
+	free(merged);
+}
+
+void shift(int *array, int *merged, size_t size)
+{
+	int middle = 0;
+
+	if (size < 2)
+		return;
+
+	middle = size / 2;
+	shift(array, merged, middle);
+	shift(array + middle, merged, size - middle);
+	merge(array, merged, middle, size);
+}
+
+void merge(int *array, int *merged, int middle, int size)
+{
+	int i, j, k;
+
+	printf("Merging...\n[left]: ");
+	print_array(array, middle);
+	printf("[right]: ");
+	print_array(array + middle, size - middle);
+
+	for (i = 0, j = middle, k = 0; k < size; k++)
+	{
+		if (j == size)
+		{
+			merged[k] = array[i];
+			i++;
+		}
+		else if (i == middle)
+		{
+			merged[k] = array[j];
+			j++;
+		}
+		else if (array[j] < array[i])
+		{
+			merged[k] = array[j];
+			j++;
+		}
+		else
+		{
+			merged[k] = array[i];
+			i++;
+		}
+	}
+	cpy(merged, array, size);
+	printf("[Done]: ");
+	print_array(merged, size);
+}
