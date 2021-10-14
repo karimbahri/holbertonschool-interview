141f96e185638867a1e9de58e7a6409efc1d6068
karim <karim-bahri-18@outlook.fr>
2021-10-14 01:34:54 +0100

    merge array

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -3,6 +3,6 @@
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge(int *array, int *merged, int middle, int size);
-void shift(int *array, int *merged, size_t size);
+void merge_wrap(int *array, int *merged, size_t size);
 void cpy(int *src, int *dest, int size);
 #endif
