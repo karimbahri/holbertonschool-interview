b67f62fafdc4bb641263208b114a7341a06d4f2f
karim <karim-bahri-18@outlook.fr>
2021-11-11 22:08:38 +0100

    sort

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,7 @@
+#ifndef __SORT_RADIX__
+#define __SORT_RADIX__
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void count_sort(int *array, size_t size, int div);
+int max(int *array, size_t size);
+#endif
