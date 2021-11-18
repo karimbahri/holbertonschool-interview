1126e0b51c57ecb0dcb770748d4407849faace13
karim <karim-bahri-18@outlook.fr>
2021-11-18 21:59:10 +0100

    island_perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -16,9 +16,9 @@ def island_perimeter(grid):
         for j in range(len_x):
             if not (grid[i][j] - 1):
                 islandPerimeter = islandPerimeter + 4
-                if (i > 0 and grid[i - 1][j] == 1):
+                if (i > 0 and not (grid[i - 1][j] - 1)):
                     islandPerimeter = islandPerimeter - 2
-                if (j > 0 and grid[i][j - 1] == 1):
+                if (j > 0 and not (grid[i][j - 1] - 1)):
                     islandPerimeter = islandPerimeter - 2
 
     return islandPerimeter
