fff669e1811d339be0277791a52fcc287e075597
karim <karim-bahri-18@outlook.fr>
2021-11-18 21:45:16 +0100

    island_perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""0-island_perimeter.py
+"""
+
+
+def island_perimeter(grid):
+    """island_perimeter:
+        args: grid
+        return: perimeter of the island described in grid
+    """
+    islandPerimeter = 0
+    len_x = len(grid[0])
+    len_y = len(grid)
+
+    for i in range(len_y):
+        for j in range(len_x):
+            if not grid[i][j] - 1:
+                islandPerimeter = islandPerimeter + 4
+                if (i > 0 or j > 0) \
+                   and (grid[i - 1][j] == 1 or grid[i][j - 1] == 1):
+                    islandPerimeter = islandPerimeter - 2
+
+    return islandPerimeter
